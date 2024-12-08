e1150dc930b4aa7bf63fbeeeb2745fc178bc1f82
Samuel D <samueldzibsads@gmail.com>
2024-12-08 00:17:01 -0600

    Feature/rebuild-login feat: add service_locator and config firebase auth, dio, httpclienthelper, flutter security storage and authapidatasource

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:plant_app/core/configs/get_it/service_locator.dart';
 import 'package:plant_app/core/configs/theme/theme.dart';
 import 'package:plant_app/core/routes/app_router.dart';
 import 'package:plant_app/domain/providers/home/home_provider.dart';
+import 'package:plant_app/firebase_options.dart';
 import 'package:provider/provider.dart';
-
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  setUpServiceLocator();
   runApp(MultiProvider(
       providers: [ChangeNotifierProvider(create: (_) => HomeProvider())],
       child: const MyApp()));
