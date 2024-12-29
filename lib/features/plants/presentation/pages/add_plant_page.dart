d073a17c08cd50c626d69b7fbd603a3639cd349f
Samuel D <samueldzibsads@gmail.com>
2024-12-28 23:32:03 -0600

    Feature/add-plant feat: add event tu bottom and remove event when loading state is true

--- lib/features/plants/presentation/pages/add_plant_page.dart
+++ lib/features/plants/presentation/pages/add_plant_page.dart
@@ -61,58 +61,65 @@ class _AddPlantPageState extends State<AddPlantPage> {
               Navigator.pop(context);
             }
           },
-          child: BlocBuilder<AddPlantCubit, AddPlantState>(
-            builder: (context, cubitState) => SingleChildScrollView(
-              child: Container(
-                padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
-                child: Column(
-                  children: [
-                    CustomTextFormField(
-                      label: context.localizations.name,
-                      onChanged: (value) =>
-                          context.read<AddPlantCubit>().onName(value),
-                    ),
-                    CustomTextFormField(
-                      label: context.localizations.seller,
-                      onChanged: (value) =>
-                          context.read<AddPlantCubit>().onSeller(value),
-                    ),
-                    CustomTextFormField(
-                      label: context.localizations.price,
-                      onChanged: (value) =>
-                          context.read<AddPlantCubit>().onPrice(value),
-                    ),
-                    SingleImageInput(
-                      label: context.localizations.image,
-                      onImageChanged: (file) =>
-                          context.read<AddPlantCubit>().onImage(file),
-                    ),
-                    CustomTextFormField(
-                      marginTop: 12,
-                      label: context.localizations.description,
-                      maxLines: 4,
-                      onChanged: (value) =>
-                          context.read<AddPlantCubit>().onDescription(value),
-                    ),
-                    CustomButtom(
-                        title: 'Guardar',
+          child: BlocBuilder<PlantsBloc, PlantsState>(
+            builder: (context, state) =>
+                BlocBuilder<AddPlantCubit, AddPlantState>(
+              builder: (context, cubitState) => SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.only(top: 24, left: 24, right: 24),
+                  child: Column(
+                    children: [
+                      CustomTextFormField(
+                        label: context.localizations.name,
+                        onChanged: (value) =>
+                            context.read<AddPlantCubit>().onName(value),
+                      ),
+                      CustomTextFormField(
+                        label: context.localizations.seller,
+                        onChanged: (value) =>
+                            context.read<AddPlantCubit>().onSeller(value),
+                      ),
+                      CustomTextFormField(
+                        label: context.localizations.price,
+                        onChanged: (value) =>
+                            context.read<AddPlantCubit>().onPrice(value),
+                      ),
+                      SingleImageInput(
+                        label: context.localizations.image,
+                        onImageChanged: (file) =>
+                            context.read<AddPlantCubit>().onImage(file),
+                      ),
+                      CustomTextFormField(
                         marginTop: 12,
-                        onPressed: () {
-                          final PlantRequest request = PlantRequest(
-                            name: cubitState.name,
-                            arrivalDate: DateTime.now(),
-                            description: cubitState.description,
-                            photo: cubitState.image!,
-                            price: cubitState.price,
-                            sellerName: cubitState.seller,
-                            update: [],
-                            photoUrl: '',
-                          );
-                          context.read<PlantsBloc>().add(SavePlantEvent(
-                                request: request,
-                              ));
-                        })
-                  ],
+                        label: context.localizations.description,
+                        maxLines: 4,
+                        onChanged: (value) =>
+                            context.read<AddPlantCubit>().onDescription(value),
+                      ),
+                      CustomButtom(
+                          title: state.savePlantProgressStatus.isLoading
+                              ? ''
+                              : 'Guardar',
+                          marginTop: 12,
+                          onPressed: state.savePlantProgressStatus.isLoading
+                              ? () {}
+                              : () {
+                                  final PlantRequest request = PlantRequest(
+                                    name: cubitState.name,
+                                    arrivalDate: DateTime.now(),
+                                    description: cubitState.description,
+                                    photo: cubitState.image!,
+                                    price: cubitState.price,
+                                    sellerName: cubitState.seller,
+                                    update: [],
+                                    photoUrl: '',
+                                  );
+                                  context.read<PlantsBloc>().add(SavePlantEvent(
+                                        request: request,
+                                      ));
+                                })
+                    ],
+                  ),
                 ),
               ),
             ),
